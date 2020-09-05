e0fb3fc54b708322117af0f835644b324515bb7c
Nelson <Cruz>
2020-09-04 22:01:46 -0600

    Ingreso validacion usuario
    se crea el servicio web para ingresar hacer login y validacion de usuario

--- src/main/java/com/ayd1/proyectoclase/controller/inte/AydUsuarioInt.java
+++ src/main/java/com/ayd1/proyectoclase/controller/inte/AydUsuarioInt.java
@@ -4,8 +4,11 @@ package com.ayd1.proyectoclase.controller.inte;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.ayd1.proyectoclase.model.LoginDto;
 
 
 @CrossOrigin
@@ -15,5 +18,9 @@ public interface AydUsuarioInt {
 	
 	@GetMapping(path="/")
 	ResponseEntity<?> getUsuarios();
+	
+	@GetMapping(path = "/login") 
+	ResponseEntity<?> login(@RequestBody LoginDto loginDto);
+	
 	
 }
